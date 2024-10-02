038c5e4a401c389206fb01fc6d9a84f8d01de3b6
karthikn1997 <karthikn951@gmail.com>
2024-10-02 15:26:32 +0530

    hh

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import Hero from '../Home-Components/Hero'
-import HomeProducts from '../Home-Components/HomeProducts'
-// import Banner from '../Home-Components/Banner'
+import React, { useEffect } from 'react';
+import Hero from '../Home-Components/Hero';
+import HomeProducts from '../Home-Components/HomeProducts';
 
 const Home = () => {
+  // Scroll to top on component mount
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []); // Empty dependency array ensures it runs only on mount
+
   return (
     <div className="flex flex-col justify-center items-center overflow-hidden" data-aos="fade-up">
       <Hero />
       <HomeProducts />
-      {/* <Banner/> */}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
