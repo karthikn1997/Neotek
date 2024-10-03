1663601b4c96ff83d2224de5fa6e7b12e560cfea
karthikn1997 <karthikn951@gmail.com>
2024-10-03 09:12:22 +0530

    dsfds

--- src/Home-Components/HomeAbout.jsx
+++ src/Home-Components/HomeAbout.jsx
@@ -9,7 +9,7 @@ const HomeAbout = () => {
                     <h1 className='uppercase text-sm md:text-lg tracking-wider text-gray-300'>Neotek Sorting International</h1>
                     <p className='tracking-wider text-white text-3xl md:text-4xl font-semibold text-center lg:text-left' style={{ textShadow: "1px 1px 0px black" }}>Global Intelligent Sorting </p>
                     <p className='text-white text-lg my-4 text-center lg:text-left'>Innovative sorting solutions for industries worldwide, ensuring precision and efficiency in every operation. Empowering businesses with cutting-edge technology for a sustainable future.</p>
-                    <button className=' py-2 px-6 border-2 border-white text-white text-sm md:text-xl tracking-wider rounded-3xl hover:bg-gray-200 hover:text-[#5F86BF] hover:border-gray-500 font-semibold'>About Us</button>
+                    <button className=' py-2 px-6 border-2 border-white text-white text-lg md:text-xl tracking-wider rounded-3xl hover:bg-gray-200 hover:text-[#5F86BF] hover:border-gray-500 font-semibold'>About Us</button>
                 </div>
             </div>
             <div className='w-full lg:w-[50%] h-[40vh] sm:h-[60vh] overflow-hidden'>
