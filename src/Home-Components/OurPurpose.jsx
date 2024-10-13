bffa157f5852b2ba4e4b0248ebac6990208c44d3
karthikn1997 <karthikn951@gmail.com>
2024-10-13 09:35:26 +0530

    ss

--- src/Home-Components/OurPurpose.jsx
+++ src/Home-Components/OurPurpose.jsx
@@ -5,13 +5,13 @@ const OurPurpose = () => {
     return (
         <section className="w-full mx-auto p-2 sm:p-6">
             <div className="flex flex-col xl:flex-row w-full py-4 lg:py-10 text-left bg-slate-100 overflow-hidden items-center justify-between">
-                
-                {/* Left Side: Image */}
-                <div className="w-full xl:w-1/2 h-64 md:h-auto overflow-hidden">
-                    <img 
-                        src={Bg} 
-                        alt="Our Purpose" 
-                        className="w-[92%] h-full object-cover mx-auto" 
+
+                {/* Left Side: Image for larger screens */}
+                <div className="hidden xl:block w-full xl:w-1/2 h-64 md:h-auto overflow-hidden">
+                    <img
+                        src={Bg}
+                        alt="Our Purpose"
+                        className="w-[92%] h-full object-cover mx-auto"
                     />
                 </div>
 
@@ -28,6 +28,16 @@ const OurPurpose = () => {
                     <p className="text-gray-700 text-sm sm:text-lg leading-relaxed mb-4">
                         At Neotek Sorting, we are committed to ensuring that food products, such as nuts, rice, wheat, and other essential commodities, meet the highest quality standards. Our purpose is to revolutionize the food sorting industry by implementing advanced sorting technologies that enhance efficiency, reduce waste, and deliver consistent, high-quality products to our customers.
                     </p>
+
+                    {/* Image for mobile view */}
+                    <div className="block xl:hidden w-full h-64 mb-4 md:mb-6 overflow-hidden">
+                        <img
+                            src={Bg}
+                            alt="Our Purpose"
+                            className="w-full h-full object-cover mx-auto"
+                        />
+                    </div>
+
                     <p className="text-gray-700 text-sm sm:text-lg leading-relaxed mb-4">
                         We understand that the quality of food is critical for healthy living, and we are dedicated to making sure that every grain and nut is carefully sorted, ensuring only the best reaches our consumers. With innovation at the heart of our processes, we strive to improve the safety and reliability of the food supply chain.
                     </p>
